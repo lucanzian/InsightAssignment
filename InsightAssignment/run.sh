7baa18c77250dad5593e9b48487d84a1971e3bee
lucanzian <luca.canzian@gmail.com>
2015-03-09 06:00:29 +0000

    Create run.sh

---
+++ InsightAssignment/run.sh
@@ -0,0 +1,13 @@
+# Move to the correct folder
+cd src/
+
+# Make sure that all the programs have the proper permissions
+chmod a+x Main.java
+chmod a+x WordCountManager.java
+chmod a+x RunningMedianManager.java
+
+# Compile and run
+javac Main.java
+java Main
+
+
